eb2fa0404744b42d9a9e9dd85419d76cc662edde
Salvador511 <salvadororozcogon511@gmail.com>
2025-02-16 22:59:54 -0600

    feat: new version

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,7 @@
 export { default as Button } from './Button/index'
 export { default as Sidebar } from './Sidebar/index'
-export { default as UserCard } from './UserCard/UserCard'
+export { default as UserCard } from './UserCard/index'
+export { default as PatientButton } from './PatientButton/index'
+export { default as AppointmentCard } from './AppointmentCard/index'
+export { default as CloseBtn } from './CloseBtn/index'
+export { default as Modal } from './Modal/index'
